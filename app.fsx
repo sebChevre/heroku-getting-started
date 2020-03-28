7eec090d0eb61503f4a9cf3c89171aad7d9f27fb
Sébastien Chèvre <seb.chevre@gmail.com>
2020-03-28 20:14:35 +0100

    Update app.fsx

--- app.fsx
+++ app.fsx
@@ -7,8 +7,8 @@ Paket.Dependencies.Install (System.IO.File.ReadAllText "paket.dependencies")
 
 //---------------------------------------------------------------------
 
-#I "packages/Suave/lib/net40"
-#r "packages/Suave/lib/net40/Suave.dll"
+#I "packages/Suave/lib/net471"
+#r "packages/Suave/lib/net471/Suave.dll"
 
 open System
 open Suave                 // always open suave
